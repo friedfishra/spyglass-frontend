1038c6cbfddc4a835a22a55c6ce15f3badafc9d7
Atul Mishra <69535893+friedfishra@users.noreply.github.com>
2022-08-29 16:00:10 -0400

    added modules to app.module

--- spyglass-webapp/src/app/app.module.ts
+++ spyglass-webapp/src/app/app.module.ts
@@ -6,15 +6,66 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { GoalsComponent } from './goals/goals.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from '@angular/common';
+
+import {CheckboxModule} from 'primeng/checkbox';
+import {CalendarModule} from 'primeng/calendar';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {DialogModule} from 'primeng/dialog';
+import {Menubar, MenubarModule} from 'primeng/menubar';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {DropdownModule} from 'primeng/dropdown';
+import {RippleModule} from 'primeng/ripple';
+import {CardModule} from 'primeng/card';
+import {AutoCompleteModule } from "primeng/autocomplete";
+import {ToolbarModule} from 'primeng/toolbar';
+import { MenuItem } from 'primeng/api';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {OverlayPanelModule} from 'primeng/overlaypanel';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {TabViewModule} from 'primeng/tabview';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {MessagesModule} from 'primeng/messages';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    GoalsComponent
+    GoalsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DatePipe,
+    CheckboxModule,
+    CalendarModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    DialogModule,
+    MenubarModule,
+    SplitButtonModule,
+    DropdownModule,
+    RippleModule,
+    CardModule,
+    AutoCompleteModule,
+    ToolbarModule,
+    ProgressSpinnerModule,
+    OverlayPanelModule,
+    InputNumberModule,
+    TabViewModule,
+    SelectButtonModule,
+    MessagesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
